806a164a3e72f9297c0d80d5c490d40f82b00490
Fadli Rivansyah <99380580+Fadli-Rivansyah@users.noreply.github.com>
2023-11-10 17:08:18 +0700

    Delete Main.py

--- Main.py
+++
@@ -1,4 +0,0 @@
-#tipe data => string, integer, boolean, float
-#casting(merubah tipe data) => bool(), int(), float(), str()
-#aritmatika => penjumlahan(+), pengurangan(-), pembagian(/), perkalian(*), modulus(%), perpangkatan(**), pembulatan(//)
-#aritmatika menganut priortias operator => () , eksponen, (perkalian, pembagian, modulo, float), penambahan, pengurangan.
